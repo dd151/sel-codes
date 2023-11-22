4283f60adaf8c2719e85c7cab4e6a459e8c9d338
dd151 <dasdeep2116@gmail.com>
2023-11-23 01:59:56 +0530

    Added Screenshots for Visual Evidence

--- filter-01/src/main/java/dd151/filter_01/FilterHandlingAssignment.java
+++ filter-01/src/main/java/dd151/filter_01/FilterHandlingAssignment.java
@@ -1,10 +1,18 @@
 package dd151.filter_01;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -21,25 +29,34 @@ public class FilterHandlingAssignment {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://www.t-mobile.com/tablets");
 
-		Thread.sleep(2000);
+		Thread.sleep(5000);
 
 		try {
-//			selectFilter("Brands", "Apple", "TCL");
-//			selectFilter("Deals", "New", "Special offer");
-//			selectFilter("Operating System", "iPadOS", "Android");
-			selectFilter("Brands", "all");
+			selectFilter("Brands", "Apple", "TCL");
+			selectFilter("Deals", "New", "Special offer");
+			selectFilter("Operating System", "iPadOS", "Android");
+//			selectFilter("Brands", "all");
+			
+			//Get Screenshot for better visual evidence
+			getScreenshot();
+		} catch (Exception e) {
+			e.printStackTrace();
 		} finally {
 			Thread.sleep(2000);
 			driver.quit();
 		}
 	}
 
-	public static void selectFilter(String filterName, String... options) {
+	public static void selectFilter(String filterName, String... options) throws IOException {
 
+		//Clicking on the Filter
 		WebElement filterEl = driver
 				.findElement(By.xpath("//legend[contains(text(),'" + filterName + "')]//ancestor::fieldset"));
 		filterEl.click();
 
+		
+		//If "all" is sent as an argument -> Iterate through all possible options and click
+		//Else iterate over the options passed in arguments -> Handled Valid and Invalid Options [Using Optional class]
 		if (options.length == 1 && options[0].equals("all")) {
 			List<WebElement> optionsElList = driver
 					.findElements(By.xpath("//input[@name='" + filterName + "']/parent::span"));
@@ -50,5 +67,14 @@ public class FilterHandlingAssignment {
 				Optional.ofNullable(driver.findElement(optionXpath)).ifPresent(e -> e.click());
 			}
 		}
+	}
+
+	public static void getScreenshot() throws IOException {
+		((JavascriptExecutor) driver).executeScript("window.scrollBy(0,505)");
+		Date date = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat("YYYY-MM-DD-hh-mm-ss-SSS");
+		String formattedDate = formatter.format(date);
+		File op = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(op, new File(System.getProperty("user.dir") + "\\screenshots\\" + formattedDate + ".png"));
 	}
 }
